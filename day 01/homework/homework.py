6dcf5a87a9bffddeec1f1aabef5be81217d7e012
salom3e <salomemaisuradze2010@gmail.com>
2024-10-17 22:45:30 +0400

    second commit

--- day 01/homework/homework.py
+++ day 01/homework/homework.py
@@ -1,12 +1,12 @@
-name = "salome"
-surname = "maisuradze"
+name = "irakli"
+surname = "chkoidze"
 
 #print(name)
 #The print function is passed an object to be displayed on the screen
 
 
-name = "salome"
-surname = "maisuradze"
+name = "irakli"
+surname = "chkoidze"
 age = 14 
 height = 58.8
 
