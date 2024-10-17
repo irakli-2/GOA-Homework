d77be4e31f7c1f0b47f99a93a2622256f8e1538c
salom3e <salomemaisuradze2010@gmail.com>
2024-10-17 22:47:23 +0400

    homework.py

--- day 01/homework/homework.py
+++ day 01/homework/homework.py
@@ -1,12 +1,12 @@
-name = "irakli"
-surname = "chkoidze"
+name = "salome"
+surname = "maisuradze"
 
 #print(name)
 #The print function is passed an object to be displayed on the screen
 
 
-name = "irakli"
-surname = "chkoidze"
+name = "salome"
+surname = "maisuradze"
 age = 14 
 height = 58.8
 
